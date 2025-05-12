29b31d4127e74ce690af8b544b080c1e39dc7cf1
LoejeeMan <72646329+GuyWithBag@users.noreply.github.com>
2025-05-12 11:00:09 +0800

    fix: capitalize date

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
+import { CreateEventTable } from "@/components/create-event-table/CreateEventTable";
 
 export default function Home() {
-  return (
-    <div>
-      Admin Side
-    </div>
-  );
+	return (
+		<div className="p-5">
+			<CreateEventTable />
+		</div>
+	);
 }
