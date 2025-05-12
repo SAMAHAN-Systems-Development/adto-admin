6fcd511d9d65abb9f07715ac3f61d9f48b211605
LoejeeMan <72646329+GuyWithBag@users.noreply.github.com>
2025-05-12 11:29:09 +0800

    Revert "fix: capitalize date"
    This reverts commit 29b31d4127e74ce690af8b544b080c1e39dc7cf1.

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,8 @@
-import { CreateEventTable } from "@/components/create-event-table/CreateEventTable";
 
 export default function Home() {
-	return (
-		<div className="p-5">
-			<CreateEventTable />
-		</div>
-	);
+  return (
+    <div>
+      Admin Side
+    </div>
+  );
 }
