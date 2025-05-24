71c6ca64115850d1234ce493201c215bc850c512
LoejeeMan <72646329+GuyWithBag@users.noreply.github.com>
2025-05-24 20:23:55 +0800

    refactor: Put this in the columns part of this file in a separate file in the columns folder

--- components/create-event-table/CreateEventTable.tsx
+++ components/create-event-table/CreateEventTable.tsx
@@ -43,182 +43,9 @@ import {
 	TableHeader,
 	TableRow,
 } from "@/components/ui/table";
-import { AteneoEvent, data } from "../columns/events-sample";
-
-// const data: AteneoEvent[] = [
-// 	{
-// 		id: "m5gr84i9",
-// 		amount: 316,
-// 		status: "success",
-// 		email: "ken99@example.com",
-// 	},
-// 	{
-// 		id: "3u1reuv4",
-// 		amount: 242,
-// 		status: "success",
-// 		email: "Abe45@example.com",
-// 	},
-// 	{
-// 		id: "derv1ws0",
-// 		amount: 837,
-// 		status: "processing",
-// 		email: "Monserrat44@example.com",
-// 	},
-// 	{
-// 		id: "5kma53ae",
-// 		amount: 874,
-// 		status: "success",
-// 		email: "Silas22@example.com",
-// 	},
-// 	{
-// 		id: "bhqecj4p",
-// 		amount: 721,
-// 		status: "failed",
-// 		email: "carmella@example.com",
-// 	},
-// ];
-
-export const columns: ColumnDef<AteneoEvent>[] = [
-	{
-		id: "select",
-		header: ({ table }) => (
-			<Checkbox
-				checked={
-					table.getIsAllPageRowsSelected() ||
-					(table.getIsSomePageRowsSelected() && "indeterminate")
-				}
-				onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
-				aria-label="Select all"
-				className="border border-secondary-200"
-			/>
-		),
-		cell: ({ row }) => (
-			<Checkbox
-				checked={row.getIsSelected()}
-				onCheckedChange={(value) => row.toggleSelected(!!value)}
-				aria-label="Select row"
-				className="border border-secondary-200"
-			/>
-		),
-		enableSorting: false,
-		enableHiding: false,
-	},
-	{
-		accessorKey: "name",
-		header: ({ column }) => {
-			return (
-				<Button
-					variant="ghost"
-					onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
-				>
-					Event Name
-					<ArrowUpDown />
-				</Button>
-			);
-		},
-		cell: ({ row }) => {
-			console.log(row.original);
-			return (
-				<div className="flex flex-row gap-2 items-center">
-					<Circle fill="var(--secondary-400)" stroke="var(--secondary-400)" />
-					<div className="flex flex-col gap-">
-						<h3 className="capitalize">{row.getValue("name")}</h3>
-						<h4 className="text-xs font-light">{row.original.events} events</h4>
-					</div>
-				</div>
-			);
-		},
-	},
-	{
-		accessorKey: "date",
-		header: ({ column }) => {
-			return (
-				<Button
-					variant="ghost"
-					onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
-				>
-					Date
-					<ArrowUpDown />
-				</Button>
-			);
-		},
-		cell: ({ row }) => <div className="capitalize">{row.getValue("date")}</div>,
-	},
-	{
-		accessorKey: "registration",
-		header: ({ column }) => {
-			return (
-				<Button
-					variant="ghost"
-					onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
-				>
-					Registration Status
-					<ChevronsLeftRight />
-				</Button>
-			);
-		},
-		cell: ({ row }) => {
-			const registration: string = row.getValue("registration");
-			return (
-				<div className="flex flex-row gap-1 items-center max-md:justify-center">
-					{registration === "Close" ? (
-						<CircleX size={24} fill="var(--destructive)" stroke="white" />
-					) : (
-						<CircleCheck size={24} fill="var(--secondary-300)" stroke="white" />
-					)}
-					<div className="capitalize">{registration}</div>
-				</div>
-			);
-		},
-	},
-	{
-		accessorKey: "status",
-		header: () => <div className="text-right">Event Status</div>,
-		cell: ({ row }) => {
-			const status: string = row.getValue("status");
-			const statusColor =
-				status === "Approved"
-					? "var(--secondary-200)"
-					: status === "Rejected"
-					? "var(--destructive)"
-					: "black";
-			return (
-				<div
-					className="text-right font-medium flex justify-center"
-					style={{ color: statusColor }}
-				>
-					{status}
-				</div>
-			);
-		},
-	},
-	{
-		id: "actions",
-		enableHiding: false,
-		cell: ({ row }) => {
-			const payment = row.original;
-
-			return (
-				<DropdownMenu>
-					<DropdownMenuTrigger asChild>
-						<Button variant="ghost" className="h-8 w-8 p-0">
-							<span className="sr-only">Open menu</span>
-							<MoreHorizontal />
-						</Button>
-					</DropdownMenuTrigger>
-					<DropdownMenuContent
-						align="end"
-						className="text-secondary-200 border border-secondary-200"
-					>
-						<DropdownMenuItem>View Edits</DropdownMenuItem>
-						<DropdownMenuItem>Details</DropdownMenuItem>
-						<DropdownMenuItem>Archive</DropdownMenuItem>
-					</DropdownMenuContent>
-				</DropdownMenu>
-			);
-		},
-	},
-];
+import { AteneoEvent } from "@/lib/types/AteneoEvent";
+import { data } from "@/components/columns/events-sample";
+import { createAdminTableColumns } from "@/components/columns/create-admin-table-columns";
 
 export function CreateEventTable() {
 	const [sorting, setSorting] = React.useState<SortingState>([]);
@@ -231,7 +58,7 @@ export function CreateEventTable() {
 
 	const table = useReactTable({
 		data,
-		columns,
+		columns: createAdminTableColumns,
 		onSortingChange: setSorting,
 		onColumnFiltersChange: setColumnFilters,
 		getCoreRowModel: getCoreRowModel(),
@@ -301,7 +128,7 @@ export function CreateEventTable() {
 						) : (
 							<TableRow>
 								<TableCell
-									colSpan={columns.length}
+									colSpan={createAdminTableColumns.length}
 									className="h-24 text-center"
 								>
 									No results.
