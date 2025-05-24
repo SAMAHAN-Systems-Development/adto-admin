870fed8d2010a44e4e75af9bfb8c50cf8701b71f
derickeroy2123 <derickeroy2123@gmail.com>
2025-05-24 16:08:39 +0800

    feat: add archive event input and button

--- app/(main)/events/page.tsx
+++ app/(main)/events/page.tsx
@@ -1,9 +1,31 @@
-import React from 'react'
+"use client";
 
-const EventsPage = () => {
-    return (
-        <div>EventsPage</div>
-    )
+import { archiveEventService } from "@/client/services/archiveEventService";
+import { useState } from "react";
+
+/**
+ * The code below is temporary. Still waiting for events component.
+ * @todo Integrate with events list.
+ */
+export default function EventsPage() {
+  const [id, setId] = useState("");
+
+  async function handleArchive() {
+    await archiveEventService(id);
+  }
+
+  return (
+    <div>
+      <input
+        className="border"
+        type="text"
+        placeholder="Enter Event ID"
+        required
+        onChange={(e) => setId(e.target.value)}
+      />
+      <button className="border" onClick={handleArchive}>
+        Archive
+      </button>
+    </div>
+  );
 }
-
-export default EventsPage
