bba17ff3a99006401673d80ec9c10004e0380e9b
hamllock <h4mlock@gmail.com>
2025-10-12 01:13:47 +0800

    feat: restore CreateEventRequest interface and add UpdateEventRequest interface

--- lib/types/requests/CreateEventRequest.ts
+++
@@ -1,9 +0,0 @@
-export interface CreateEventRequest {
-  name: string;
-  description: string;
-  dateStart: string;
-  dateEnd: string;
-  isRegistrationOpen?: boolean;
-  isRegistrationRequired?: boolean;
-  isOpenToOutsiders?: boolean;
-}
