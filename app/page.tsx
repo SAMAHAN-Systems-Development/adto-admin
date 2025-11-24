523b7863f791fe1fdedd40cd633dfc4f575f8cff
Aycees <adolfocedric@gmail.com>
2025-11-24 21:17:28 +0800

    added /login as landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,12 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
 export default function Home() {
-  return <div>Admin Side</div>;
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push("/login");
+  }, [router]);
 }
