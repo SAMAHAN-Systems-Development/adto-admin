f2a1117ba2aedc904d7ada54b5de3cb1ef9dd7d6
Carlo Allan Laynes <carloallanlaynes@gmail.com>
2026-02-01 18:29:26 +0800

    feat: implement overview section in dashboard page

--- app/(main)/dashboard/page.tsx
+++ app/(main)/dashboard/page.tsx
@@ -1,7 +1,13 @@
+import { OverviewSection } from "@/components/features/dashboard/overview-section";
 import React from "react";
 
 const DashboardPage = () => {
-  return <div>DashboardPage</div>;
+  return (
+    <div className="container mx-auto py-6">
+      <h1 className="text-5xl font-bold my-10">Hello, SAMAHAN SYSDEV</h1>
+      <OverviewSection />
+    </div>
+  );
 };
 
 export default DashboardPage;
