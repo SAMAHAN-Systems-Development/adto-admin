2f38fe5a488e9b18ccb17b233b1c4fc796968df7
Carlo Allan Laynes <carloallanlaynes@gmail.com>
2026-02-04 00:46:58 +0800

    feat: add caching to overview query

--- lib/api/queries/dashboardQueries.ts
+++ lib/api/queries/dashboardQueries.ts
@@ -5,5 +5,6 @@ export const useDashboardOverviewQuery = () => {
   return useQuery({
     queryKey: ["dashboard-overview"],
     queryFn: getDashboardOverview,
+    staleTime: 60 * 1000, // 1 minute
   });
 };
